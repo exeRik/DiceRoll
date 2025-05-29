669e5cd4045793f27e1956780842a0222ecaf185
Smarika <149926359+exeRik@users.noreply.github.com>
2025-05-29 22:43:12 +0545

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ btnRoll.addEventListener('click', function () {
     console.log(randomNo);
     // display dice roll images
     dice.classList.remove('hidden');
-    dice.src =`DiceRoll/Images/dice-${randomNo}.png`;
+    dice.src =`DiceRoll/DiceRoll/Images/dice-${randomNo}.png`;
     // // check for 1
     if(randomNo!= 1)
     {
@@ -67,7 +67,7 @@ btnHold.addEventListener('click', function(){
     document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
     document.querySelector(`#current--${activePlayer}`).textContent = 0 ;
 
-    if (score[activePlayer]>=10){
+    if (score[activePlayer]>=100){
         playing = false;
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
         dice.classList.add('hidden');
