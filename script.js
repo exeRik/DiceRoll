5123ecfb3b776d052c1083bc20853458264aa8c5
Smarika <149926359+exeRik@users.noreply.github.com>
2025-05-29 23:00:54 +0545

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,8 @@ btnRoll.addEventListener('click', function () {
         let randomNo = Math.trunc(Math.random() * 6) + 1;
 
         dice.classList.remove('hidden');
-        dice.src = `DiceRoll/DiceRoll/Images/dice-${randomNo}.png`;
+        dice.src = `DiceRoll/DiceRoll/Images/dice-1.png`;
+        
 
         if (randomNo !== 1) {
             currentScore += randomNo;
